c4e66a6b74e1f4919093fc6c89bce9c12f662b89
Carlo Parata <carlo.parata@st.com>
2021-09-02 19:04:01 +0200

    Initial release

--- examples/X_NUCLEO_NFC05A1_HelloWorld/X_NUCLEO_NFC05A1_HelloWorld.ino
+++ examples/X_NUCLEO_NFC05A1_HelloWorld/X_NUCLEO_NFC05A1_HelloWorld.ino
@@ -1,23 +1,34 @@
-/******************************************************************************
-  * \attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2021 STMicroelectronics</center></h2>
-  *
-  * Licensed under ST MIX MYLIBERTY SOFTWARE LICENSE AGREEMENT (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        www.st.com/mix_myliberty
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
-  * AND SPECIFICALLY DISCLAIMING THE IMPLIED WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-******************************************************************************/
+/**
+ ******************************************************************************
+ * @file    X_NUCLEO_NFC05A1_HelloWorld.ino
+ * @author  AST
+ * @version V1.0.0
+ * @date    2 September 2021
+ * @brief   Arduino test application for the STMicrolectronics X-NUCLEO-NFC05A1
+ *          NFC reader/writer expansion board.
+ *          This application makes use of C++ classes obtained from the C
+ *          components' drivers.
+ ******************************************************************************
+ * \attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2021 STMicroelectronics</center></h2>
+ *
+ * Licensed under ST MIX MYLIBERTY SOFTWARE LICENSE AGREEMENT (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *        www.st.com/mix_myliberty
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
+ * AND SPECIFICALLY DISCLAIMING THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************
+ */
 
 /*! \file
  *
